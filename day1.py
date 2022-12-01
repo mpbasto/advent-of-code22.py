8023190f991c0ca537c7b4df862bb2f552ff21f9
Marta Basto <87577613+mpbasto@users.noreply.github.com>
2022-12-01 18:21:23 +0000

    Add Day 1 solution and input

---
+++ day1.py
@@ -0,0 +1,8 @@
+with open('inputs/day1-input.txt', 'r') as data:
+    elves = []
+    for elf in data.read().split("\n\n"):
+        elves.append(sum(map(int, elf.split())))
+
+elves = sorted(elves, reverse=True) 
+print(f'Part one: {elves[0]}')
+print(f'Part two: {sum(elves[:3])}')
